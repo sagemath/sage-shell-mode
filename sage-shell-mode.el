f8ca69ac69d93d127de68dfcf6cc7764b08aac88
Sho Takemori <stakemorii@gmail.com>
2014-09-24 23:17:01 +0900

    Fix indentation

--- sage-shell-mode.el
+++ sage-shell-mode.el
@@ -833,21 +833,21 @@ Match group 1 will be replaced with devel/sage-branch")
   "If FILENAME is in site-packages, current branch version, else FILENAME."
   (save-match-data
     (let* ((match (string-match sage-shell:site-packages-regexp filename)))
-           (sage-shell:aif (and filename match
-                          (cl-loop for a in '("devel" "src")
-                                   if (file-exists-p
-                                       (expand-file-name
-                                        a (sage-shell:sage-root)))
-                                return a))
-               (let* ((base (concat (substring filename 0
-                                               (match-beginning 1))
-                                    it))
-                      (branch (cond ((string= "devel" it)
-                                     (or (file-symlink-p (concat base "/sage"))
-                                         "/sage"))
-                                    (t ""))))
-                 (concat base branch (substring filename (match-end 1))))
-             filename))))
+      (sage-shell:aif (and filename match
+                           (cl-loop for a in '("devel" "src")
+                                    if (file-exists-p
+                                        (expand-file-name
+                                         a (sage-shell:sage-root)))
+                                    return a))
+          (let* ((base (concat (substring filename 0
+                                          (match-beginning 1))
+                               it))
+                 (branch (cond ((string= "devel" it)
+                                (or (file-symlink-p (concat base "/sage"))
+                                    "/sage"))
+                               (t ""))))
+            (concat base branch (substring filename (match-end 1))))
+        filename))))
 
 (defcustom sage-shell:prefer-development-file-p t
   "If non nil, prefer a source file in devel directory rather than site-packages directory."
