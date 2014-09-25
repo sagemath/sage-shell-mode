19353d01b5e4e83e0c6f1dd7bdfed76e1d473206
Sho Takemori <stakemorii@gmail.com>
2014-09-25 22:58:43 +0900

    Use noflet for test.

--- Cask
+++ Cask
@@ -4,4 +4,5 @@
 (package-file "sage-shell-mode.el")
 (depends-on "cl-lib")
 (development
- (depends-on "ert"))
+ (depends-on "ert")
+ (depends-on "noflet"))
--- test/sage-shell-mode-test.el
+++ test/sage-shell-mode-test.el
@@ -1,9 +1,9 @@
 (require 'ert)
 (require 'sage-shell-mode)
-
+(require 'noflet)
 (ert-deftest sage-shell-mode:sage-shell:development-version-test ()
-  (should (string=
-           (sage-shell:development-version
-            "/usr/lib/sagemath/local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py")
-           "/usr/lib/sagemath/src/sage/rings/function_field/function_field.py")))
-
+  (noflet ((sage-shell:sage-root () "/usr/lib/sagemath/"))
+          (should (string=
+                   (sage-shell:development-version
+                    "/usr/lib/sagemath/local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py")
+                   "/usr/lib/sagemath/src/sage/rings/function_field/function_field.py"))))
