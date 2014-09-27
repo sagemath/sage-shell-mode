316efeddef8c7c6002c720f826d674239f460a78
Sho Takemori <stakemorii@gmail.com>
2014-09-27 23:50:46 +0900

    Modified sage-shell-help:file-type-make-button.

--- sage-shell-mode.el
+++ sage-shell-mode.el
@@ -1740,11 +1740,18 @@ python-mode"
      beg end
      'sage-shell:file file 'sage-shell:line line
      'sage-shell:cont cont
+     'sage-shell:proc-buf sage-shell:process-buffer
      'action (lambda (button)
-               (let ((linenum (button-get button 'sage-shell:line))
-                     (cont (button-get button 'sage-shell:cont)))
-                 (find-file-other-window
-                  (button-get button 'sage-shell:file))
+               (let* ((linenum (button-get button 'sage-shell:line))
+                      (cont (button-get button 'sage-shell:cont))
+                      (proc-buf (button-get button 'sage-shell:proc-buf))
+                      (file (button-get button 'sage-shell:file))
+                      (buf (find-file-noselect file)))
+                 (with-current-buffer buf
+                   (setq sage-shell:process-buffer proc-buf))
+                 (select-window (get-buffer-window proc-buf))
+                 (pop-to-buffer buf)
+                 (select-window (get-buffer-window buf))
                  (when linenum
                    (goto-char (point-min))
                    (forward-line (1- linenum))
