7826e9b98fe625ce7c92c18ae8ade657435888ea
Sho Takemori <stakemorii@gmail.com>
2015-03-01 12:10:52 +0900

    Renaming.

--- test/sage-shell-mode-test.el
+++ test/sage-shell-mode-test.el
@@ -1,7 +1,7 @@
 (require 'ert)
 (require 'sage-shell-mode)
 (require 'noflet)
-(ert-deftest sage-shell-mode:sage-shell:development-version-test ()
+(ert-deftest sage-shell:development-version-test ()
   (should (string=
            (sage-shell:src-version
             (expand-file-name "local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py"
