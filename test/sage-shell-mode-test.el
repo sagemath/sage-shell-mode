b14b6ab5439f23c5acce6dddd89fd34d0fd1f18a
Sho Takemori <stakemorii@gmail.com>
2015-03-01 12:00:42 +0900

    Modified sage-shell:development-version-test.

--- test/sage-shell-mode-test.el
+++ test/sage-shell-mode-test.el
@@ -2,17 +2,21 @@
 (require 'sage-shell-mode)
 (require 'noflet)
 (ert-deftest sage-shell-mode:sage-shell:development-version-test ()
-  (noflet ((sage-shell:sage-root () "/usr/lib/sagemath/"))
-          (should (string=
-                   (sage-shell:src-version
-                    "/usr/lib/sagemath/local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py")
-                   "/usr/lib/sagemath/src/sage/rings/function_field/function_field.py"))
-          (should (string=
-                   (sage-shell:src-version
-                    "/usr/lib/sagemath/local/lib/python2.7/site-packages/sage/misc/cachefunc.so")
-                   "/usr/lib/sagemath/src/sage/misc/cachefunc.pyx"))
-          (should (string=
-                   (sage-shell:site-package-version
-                    "/usr/lib/sagemath/src/sage/rings/function_field/function_field.py")
-                   "/usr/lib/sagemath/local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py"
-                   ))))
+  (should (string=
+           (sage-shell:src-version
+            (expand-file-name "local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py"
+                              (sage-shell:sage-root)))
+           (expand-file-name "src/sage/rings/function_field/function_field.py"
+                             (sage-shell:sage-root))))
+  (should (string=
+           (sage-shell:src-version
+            (expand-file-name "local/lib/python2.7/site-packages/sage/misc/cachefunc.so"
+                              (sage-shell:sage-root)))
+           (expand-file-name "src/sage/misc/cachefunc.pyx"
+                             (sage-shell:sage-root))))
+  (should (string=
+           (sage-shell:site-package-version
+            (expand-file-name "src/sage/rings/function_field/function_field.py"
+                              (sage-shell:sage-root)))
+           (expand-file-name "local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py"
+                             (sage-shell:sage-root)))))
