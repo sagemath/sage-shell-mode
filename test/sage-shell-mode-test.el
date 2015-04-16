31955f883bc5eb5c0333eb49b628a8803aead815
Sho Takemori <stakemorii@gmail.com>
2015-04-17 06:50:07 +0900

    Ignore test using sage-shell:sage-root.

--- test/sage-shell-mode-test.el
+++ test/sage-shell-mode-test.el
@@ -2,24 +2,29 @@
 (require 'sage-shell-mode)
 
 (ert-deftest sage-shell:development-version-test ()
-  (should (string=
-           (sage-shell:src-version
+  (should (or (string= (user-login-name) "travis")
+              (string=
+               (sage-shell:src-version
+                (expand-file-name "local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py"
+                                  (sage-shell:sage-root)))
+               (expand-file-name "src/sage/rings/function_field/function_field.py"
+                                 (sage-shell:sage-root)))))
+  (should (or
+           (string= (user-login-name) "travis")
+           (string=
+            (sage-shell:src-version
+             (expand-file-name "local/lib/python2.7/site-packages/sage/misc/cachefunc.so"
+                               (sage-shell:sage-root)))
+            (expand-file-name "src/sage/misc/cachefunc.pyx"
+                              (sage-shell:sage-root)))))
+  (should (or
+           (string= (user-login-name) "travis")
+           (string=
+            (sage-shell:site-package-version
+             (expand-file-name "src/sage/rings/function_field/function_field.py"
+                               (sage-shell:sage-root)))
             (expand-file-name "local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py"
-                              (sage-shell:sage-root)))
-           (expand-file-name "src/sage/rings/function_field/function_field.py"
-                             (sage-shell:sage-root))))
-  (should (string=
-           (sage-shell:src-version
-            (expand-file-name "local/lib/python2.7/site-packages/sage/misc/cachefunc.so"
-                              (sage-shell:sage-root)))
-           (expand-file-name "src/sage/misc/cachefunc.pyx"
-                             (sage-shell:sage-root))))
-  (should (string=
-           (sage-shell:site-package-version
-            (expand-file-name "src/sage/rings/function_field/function_field.py"
-                              (sage-shell:sage-root)))
-           (expand-file-name "local/lib/python2.7/site-packages/sage/rings/function_field/function_field.py"
-                             (sage-shell:sage-root)))))
+                              (sage-shell:sage-root))))))
 
 (defun sage-shell-test:temp-state
     (code int &optional pt)
