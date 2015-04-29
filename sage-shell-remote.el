6fb99e7cffe49158c2028919d331a9aefa419a45
Sho Takemori <stakemorii@gmail.com>
2015-04-30 00:25:15 +0900

    The default dir of the working dir is HOME.

--- sage-shell-remote.el
+++ sage-shell-remote.el
@@ -10,24 +10,22 @@
   "Running Sage function internal.
 SIWTCH-FUNCTION is 'no-switch, or a function with one
 argument."
-  (let* ((default-directory (sage-shell-remote:plist-to-file-name
-                             (cdr name-plist)))
+  (let* ((plist (cdr name-plist))
+         (default-directory (sage-shell-remote:-expand-fname
+                             (or (plist-get plist :default-directory)
+                                 "~/")
+                             plist))
          ;; Spwan process
          (proc-buf (sage-shell:run cmd new switch-function (car name-plist))))
     (with-current-buffer proc-buf
       (setq sage-shell:remote-name-plist name-plist))))
 
-(defun sage-shell-remote:plist-to-file-name (plist)
+(defun sage-shell-remote:-expand-fname (f plist)
   (tramp-make-tramp-file-name
    (plist-get plist :method)
    (plist-get plist :user)
    (plist-get plist :host)
-   (plist-get plist :localname)))
-
-(defun sage-shell-remote:-expand-fname (f plist)
-  (sage-shell-remote:plist-to-file-name
-   (append (list :localname f)
-           plist)))
+   f))
 
 (defun sage-shell-remote:run-sage (name)
   "Run remote Sage associated to NAME."
