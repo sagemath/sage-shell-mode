a20e6a328c08448531a766c3936496fb3af478e4
Sho Takemori <stakemorii@gmail.com>
2016-03-07 23:20:40 +0900

    Bump up verion
    [skip ci]

--- sage-shell-mode.el
+++ sage-shell-mode.el
@@ -5,7 +5,7 @@
 ;; URL: https://github.com/stakemori/sage-shell-mode
 ;; Package-Requires: ((cl-lib "0.5") (deferred "0.3.1"))
 ;; Keywords: Sage, math
-;; Version: 0.0.8
+;; Version: 0.0.9
 
 ;;; License
 ;; This program is free software; you can redistribute it and/or modify
