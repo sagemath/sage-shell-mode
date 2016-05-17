2e49bb8e21737f5d1dc59cf79fd72bfc6c68267a
Sho Takemori <stakemorii@gmail.com>
2016-05-17 14:37:32 +0900

    Remove newline chars in eldoc-string

--- sage-shell-mode.el
+++ sage-shell-mode.el
@@ -1390,8 +1390,10 @@ Match group 1 will be replaced with devel/sage-branch")
                         (format "print_def('%s', base_name=%s)"
                                 func-name base-name))
                        sage-shell:send-command-to-string
-                       sage-shell:trim-right
-                       (funcall (lambda (s) (car (last (split-string s "\n"))))))))
+                       (funcall
+                        (lambda (s)
+                          (replace-regexp-in-string (rx "\n") "" s nil t)))
+                       sage-shell:trim-right)))
             (push (cons func-name res) sage-shell:-eldoc-cache)
             res)))))))
 
