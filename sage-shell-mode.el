b6a167c256e4949fa3fa86e32bfbfd2098938be3
Sho Takemori <stakemorii@gmail.com>
2016-09-03 21:57:56 +0900

    Add comment

--- sage-shell-mode.el
+++ sage-shell-mode.el
@@ -44,6 +44,8 @@
 ;;; TODO
 ;; 1. Fix sage-shell-edit:exec-command-base when insert-command-p is non-nil.
 ;; 5. Disabel auto indent (cf. IPython's issue #9888).
+;; Should sage-shell:-adjust-window-size be added to
+;; window-configuration-change-hook?
 
 ;;; Code:
 
