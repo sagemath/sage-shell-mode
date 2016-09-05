83edfa320949c4a3742aac4426f0b050e82b46e0
Sho Takemori <stakemorii@gmail.com>
2016-09-05 09:24:03 +0900

    Advise restarting the process

--- sage-shell-mode.el
+++ sage-shell-mode.el
@@ -1746,7 +1746,10 @@ Return value is not deifned."
                (cond ((listp a)
                       (unless sage-shell:use-prompt-toolkit
                         (message
-                         "To use sage-shell-mode properly, please set `sage-shell:use-prompt-toolkit' to t."))
+                         (concat
+                          "To use sage-shell-mode properly, "
+                          "please set `sage-shell:use-prompt-toolkit' to t."
+                          " And restart the SageMath process.")))
                       (let ((args (cadr a)))
                         (apply (assoc-default
                                 (car a)
