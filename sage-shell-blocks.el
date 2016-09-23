3c3bc21d41179974aa9275a50a80dcf17edbe149
Johan Rosenkilde <jsrn@jsrn.dk>
2016-09-23 15:15:15 +0200

    GPL license

--- sage-shell-blocks.el
+++ sage-shell-blocks.el
@@ -3,14 +3,29 @@
 ;; Copyright (C) 2013-2016 Johan Rosenkilde
 
 ;; Author: Johan Rosenkilde <jsrn@jsrn.dk>
-;; Keywords: sage
+;; URL: https://github.com/stakemori/sage-shell-mode
+;; Keywords: Sage, math
+
+;;; License
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
 ;; This file adds functionality which supports structuring experimental Sage
 ;; code in "sheets", where the code is bundled in "blocks" akin to the boxes of
 ;; the Notebook. Functions are provided for convenient handling of such blocks.
-;; The file injects a few keybindings into `sage-mode' as well as
+;; The file injects a few keybindings into `sage-shell:sage-mode' as well as
 ;; `sage-shell-mode'.
 
 ;; A block is defined by a line beginning with `sage-shell-blocks:delimiter'.
